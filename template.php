6f78a597e6717b474d77f5983e301daed70d3c19
AndersonMoura87 <andersondecastromoura@gmail.com>
2025-06-05 21:38:49 -0300

    primeiro commit

---
+++ template.php
@@ -0,0 +1,28 @@
+<div class="progress-text">
+  PROGRESSO DA META PRINCIPAL<br />
+  <span id="percent-text">0%</span>
+</div>
+
+<div class="progress-bar" id="bar">
+  <div class="progress-fill" id="fill"></div>
+  <div id="mario"></div>
+  <!-- blocos -->
+  <div class="bloco" style="left: 15%;"></div>
+  <div class="bloco" style="left: 30%;"></div>
+  <div class="bloco" style="left: 45%;"></div>
+  <div class="bloco" style="left: 60%;"></div>
+  <div class="bloco" style="left: 75%;"></div>
+  <div class="bloco" style="left: 90%;"></div>
+</div>
+
+<div class="input-row">
+  Progresso (X): <input type="number" id="progresso" value="15" min="0" max="100" />
+  <button onclick="atualizar()">ATUALIZAR</button>
+</div>
+
+<div class="conquista" id="conquista">
+  <div class="conquista-text">NOVA CONQUISTA DESBLOQUEADA</div>
+  <button>DESVENDAR DADOS</button>
+</div>
+
+<div class="footer">© 2025 scentzuchepard</div>
